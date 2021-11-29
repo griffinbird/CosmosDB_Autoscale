41aca0df2d6e2c3db5566e16553abb16430cc443
griffinbird <benito.griffin@gmail.com>
2021-11-29 23:08:42 +1100

    Initial commit.

---
+++ CustomEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AutoscaleDemo
+{
+    class CartOperationEvent
+    {
+        public string id { get; set; }
+        public string CartID { get; set; }
+        public string Action { get; set; }
+        public string Item { get; set; }
+        public string Price { get; set; }
+        public string Username { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string Date { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+    class CartOperationEventBasic
+    {
+        public string id { get; set; }
+        public string CartID { get; set; }
+    }
+
+    class PaymentEvent
+    {
+        public string id { get; set; }
+        public Decimal Amount { get; set; }
+        public string TransactionType { get; set; }
+        public string Currency { get; set; }
+        public string Username { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string Date { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+}
---
+++ Util.cs
@@ -0,0 +1,68 @@
+﻿using Bogus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AutoscaleDemo
+{
+    class Util
+    {
+        static internal List<CartOperationEvent> GenerateRandomCartOperationEvents(int numberOfDocumentsPerBatch)
+        {
+            var actions = new[] { "viewed", "addedToCart", "purchased" };
+
+            // have only 10k posts
+            var faker = new Faker();
+
+            var operationEvent = new Faker<CartOperationEvent>()
+
+                //Ensure all properties have rules. 
+                .StrictMode(true)
+
+                .RuleFor(o => o.id, f => Guid.NewGuid().ToString())
+
+                //Generate event
+                .RuleFor(o => o.CartID, f => Guid.NewGuid().ToString()) //TODO: Make it an integer value
+                .RuleFor(o => o.Action, f => f.PickRandom(actions))
+
+                .RuleFor(o => o.Item, f => f.Commerce.ProductName())
+                .RuleFor(o => o.Price, f => f.Commerce.Price())
+                .RuleFor(o => o.Username, f => f.Internet.UserName())
+                .RuleFor(o => o.Country, f => f.Address.Country())
+                //.RuleFor(o => o.timestamp, f => f.Date.Between(DateTime.Now, DateTime.Now.AddDays(-1))) //3 days ago
+                .RuleFor(o => o.Address, f => f.Address.StreetAddress())
+                .RuleFor(o => o.Timestamp, f => DateTime.Now) // just today's date
+                .RuleFor(o => o.Date, (f, m) => $"{m.Timestamp.ToString("yyyy-MM-dd")}");
+
+            var events = operationEvent.Generate(numberOfDocumentsPerBatch);
+
+            return events;
+
+        }
+
+        static internal List<PaymentEvent> GenerateRandomPaymentEvent(int numberOfDocumentsPerBatch)
+        {
+            var faker = new Faker();
+
+            var paymentEvent = new Faker<PaymentEvent>()
+                .StrictMode(true)
+                //Generate event
+                .RuleFor(o => o.id, f => Guid.NewGuid().ToString())
+                .RuleFor(o => o.Amount, f => f.Finance.Amount())
+                .RuleFor(o => o.TransactionType, f => f.Finance.TransactionType())
+                .RuleFor(o => o.Currency, f => f.Finance.Currency().ToString())
+                .RuleFor(o => o.Username, f => f.Internet.UserName())
+                .RuleFor(o => o.Country, f => f.Address.Country())
+                .RuleFor(o => o.Address, f => f.Address.StreetAddress())
+                .RuleFor(o => o.Timestamp, f => DateTime.Now) // just today's date
+                .RuleFor(o => o.Date, (f, m) => $"{m.Timestamp.ToString("yyyy-MM-dd")}");
+
+            var events = paymentEvent.Generate(numberOfDocumentsPerBatch);
+
+            return events;
+
+        }
+    }
+}
+
